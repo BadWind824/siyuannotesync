decfa13dd9ff981a99aac88b291af8aaa19b85e1
BadWind824 <111963768+BadWind824@users.noreply.github.com>
2025-08-13 16:50:25 +0800

    2025/8/13 16:50:24 - 思源笔记同步

--- conf/appearance/themes/QYL/script/color/DarkRevert.js
+++
@@ -1,6 +0,0 @@
-export function initDarkRevert() {
-    document.documentElement.classList.add('QYLDarkRevert');
-}
-export function removeDarkRevert() {
-    document.documentElement.classList.remove('QYLDarkRevert');
-}
